9a94e0deb4aa38e4864dd0b4c6aa2cb35c1f6d00
wei10144 <kuowei1014@gmail.com>
2025-05-12 10:28:12 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,12 @@ const pointsToDraw = [
   76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184
 ];
 
+// 嘴唇的點索引
+const lipsPoints = [
+  61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 61, // 外嘴唇
+  78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308, 78  // 內嘴唇
+];
+
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
@@ -28,6 +34,9 @@ function modelReady() {
 function draw() {
   image(video, 0, 0, width, height);
 
+  // 繪製嘴唇線條
+  drawLips();
+
   // 繪製指定的點
   drawKeypoints();
 }
@@ -43,5 +52,23 @@ function drawKeypoints() {
       noStroke();
       ellipse(x, y, 5, 5); // 繪製紅色的點
     }
+  }
+}
+
+function drawLips() {
+  for (let i = 0; i < predictions.length; i++) {
+    const keypoints = predictions[i].scaledMesh;
+
+    // 繪製外嘴唇
+    stroke(0, 255, 0); // 綠色線條
+    strokeWeight(3); // 線條粗細為 3
+    noFill();
+    beginShape();
+    for (let j = 0; j < lipsPoints.length; j++) {
+      const index = lipsPoints[j];
+      const [x, y] = keypoints[index];
+      vertex(x, y);
+    }
+    endShape(CLOSE); // 將線條閉合
   }
 }
